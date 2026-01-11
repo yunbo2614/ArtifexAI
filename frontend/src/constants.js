d1a3821d29d2f6131230fe61ccba589c48057b6a
yunbo2614 <yunbo2614@gmail.com>
2026-01-11 09:42:46 -0800

    update frontend

---
+++ frontend/src/constants.js
@@ -0,0 +1,7 @@
+export const TOKEN_KEY = "token";
+export const BASE_URL = "https://socialai-477617-b6.uk.r.appspot.com"; //Fix here url
+export const SEARCH_KEY = {
+  all: 0,
+  keyword: 1,
+  user: 2,
+};
